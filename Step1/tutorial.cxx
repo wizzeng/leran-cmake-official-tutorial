de8e4a3f61daf40fff338ef6eb374346a2930627
wizzeng <wizzeng@tencent.com>
2023-01-09 22:43:47 +0800

    feat: complete Step1

--- Step1/tutorial.cxx
+++ Step1/tutorial.cxx
@@ -1,22 +1,24 @@
 // A simple program that computes the square root of a number
 #include <cmath>
-#include <cstdlib> // TODO 5: Remove this line
 #include <iostream>
 #include <string>
 
-// TODO 11: Include TutorialConfig.h
+// 11: Include TutorialConfig.h
+// You should configure cmake before using a file specified by cmake configure_file directive
+// cmake <rPath>
+#include "TutorialConfig.h"
 
 int main(int argc, char* argv[])
 {
   if (argc < 2) {
-    // TODO 12: Create a print statement using Tutorial_VERSION_MAJOR
+    // 12: Create a print statement using Tutorial_VERSION_MAJOR
     //          and Tutorial_VERSION_MINOR
+    std::cout << "Version: " << Tutorial_VERSION_MAJOR << "." << Tutorial_VERSION_MINOR << std::endl;
     std::cout << "Usage: " << argv[0] << " number" << std::endl;
     return 1;
   }
 
   // convert input to double
-  // TODO 4: Replace atof(argv[1]) with std::stod(argv[1])
   const double inputValue = atof(argv[1]);
 
   // calculate square root
